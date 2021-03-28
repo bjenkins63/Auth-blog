1c5572aade986032d672e86cf5f5a6a44204f31e
Brian Jenkins <studiojenkins@yahoo.com>
2021-03-28 10:31:28 -0400

    some new changes, art IS uploading!

--- app.js
+++ app.js
@@ -1,46 +1,86 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const mysql = require('mysql2');
 
-require('dotenv').config();
-
 const app = express();
+const port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
+app.use(fileUpload());
 
-//parse application middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-
-//static files
 app.use(express.static('public'));
+app.use(express.static('upload'));
 
-
-//setup handlebars
-app.engine('hbs', exphbs({extname: 'hbs' }));
+app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
-//connection port
 const pool = mysql.createPool({
-    connectionLimit : 100,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'Bernie2020',
+  database: 'BubsBlog'
 });
 
-//connect to db
 pool.getConnection((err, connection) => {
-    if(err) throw err; 
-    console.log(`connected as ID ` + connection.threadId)
+  if (err) throw err; 
+  console.log('Connected!');
+});
+
+app.get('', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log('Connected!');
+
+    connection.query('SELECT * FROM blogs', (err, rows) => {
+      connection.release();
+
+      if (!err) {
+        res.render('index', { rows });
+      }
+    });
+  });
+});
+
+app.post('', (req, res) => {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // name of the input is sampleFile
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/upload/' + sampleFile.name;
+
+  console.log(sampleFile);
+
+  // Use mv() to place file on the server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    pool.getConnection((err, connection) => {
+      if (err) throw err; // not connected
+      console.log('Connected!');
+
+      connection.query('UPDATE user SET profile_image = ? WHERE id ="1"', [sampleFile.name], (err, rows) => {
+        // Once done, release connection
+        connection.release();
+
+        if (!err) {
+          res.redirect('/');
+        } else {
+          console.log(err);
+        }
+
+      });
+    });
+
+    res.send('File uploaded!');
+  });
 });
 
 
-const routes = require('./server/routes/blog');
-app.use('/', routes);
 
-
-app.listen(PORT, () => console.log (`listening on port: ${PORT}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
